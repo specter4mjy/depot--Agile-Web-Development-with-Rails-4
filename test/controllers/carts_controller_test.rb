4a516735a2530494c298bc9d48b9826405452577
Junyang Ma <specter4mjy@gmail.com>
2016-07-29 02:08:01 +0100

    import cart appearance

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+
 class CartsControllerTest < ActionController::TestCase
   setup do
     @cart = carts(:one)
@@ -41,9 +42,10 @@ class CartsControllerTest < ActionController::TestCase
 
   test "should destroy cart" do
     assert_difference('Cart.count', -1) do
+      session[:cart_id] = @cart.id
       delete :destroy, id: @cart
     end
 
-    assert_redirected_to carts_path
+    assert_redirected_to store_path
   end
 end
